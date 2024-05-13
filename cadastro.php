32ef2338b8d87af5f15380c32e18913fe2e5c6e9
vtrpaulop <119384161+vtrpaulop@users.noreply.github.com>
2024-05-12 21:38:53 -0300

    .html alterado para .php
    já cadastrando no banco

---
+++ cadastro.php
@@ -0,0 +1,101 @@
+<?php
+require_once 'autentica.php';
+
+// Verifica se o formulário foi enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Processamento dos dados do formulário
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $senha_confirmar = $_POST['senha__confirmar'];
+
+    // Verifica se os campos obrigatórios estão preenchidos
+    if(empty($nome) || empty($email) || empty($senha) || empty($senha_confirmar)) {
+        die("Por favor, preencha todos os campos.");
+    }
+    // Hash da senha
+    $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
+
+    // Inserção dos dados no banco de dados
+    $sql = "INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email', '$senha_hash')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Cadastro realizado com sucesso!";
+    } else {
+        echo "Erro ao cadastrar: " . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="/assets/css/default.css" />
+    <link rel="stylesheet" href="/assets/css/components.css" />
+    <link rel="stylesheet" href="/assets/css/cadastro.css" />
+    <title>Cadastro</title>
+</head>
+<body>
+<section class="c-login">
+    <div class="c-screen-width">
+        <div class="c-login__section">
+            <div class="c-login__left"></div>
+            <div class="c-login__right">
+                <h1 class="c-login__title">Criar uma conta</h1>
+                <form action="cadastro.php" method="post" class="c-login__form">
+                    <div class="c-login__input">
+                        <label for="nome">Nome</label>
+                        <input
+                                type="text"
+                                name="nome"
+                                id="nome"
+                                placeholder="Nome"
+                                class="c-input"
+                        />
+                    </div>
+                    <div class="c-login__input">
+                        <label for="email">Email</label>
+                        <input
+                                type="text"
+                                name="email"
+                                id="email"
+                                placeholder="E-mail"
+                                class="c-input"
+                        />
+                    </div>
+                    <div class="c-login__input">
+                        <label for="senha">Senha</label>
+                        <input
+                                type="text"
+                                name="senha"
+                                id="senha"
+                                placeholder="Senha"
+                                class="c-input"
+                        />
+                    </div>
+                    <div class="c-login__input">
+                        <label for="senha">Confirmar a senha</label>
+                        <input
+                                type="text"
+                                name="senha__confirmar"
+                                id="senha__confirmar"
+                                placeholder="Confimar a senha"
+                                class="c-input"
+                        />
+                    </div>
+                    <input
+                            type="submit"
+                            value="Cadastrar"
+                            class="c-button__primary"
+                    />
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
