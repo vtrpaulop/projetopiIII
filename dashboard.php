1a08344f11e7392e61ab8f61a1dd7aeabe8a6551
jonatasbaldan <jonatasbzc@gmail.com>
2024-05-20 20:47:58 -0300

    Feature: Desaclopado o menu principal do dashboard para um componente

--- dashboard.php
+++ dashboard.php
@@ -15,20 +15,7 @@ $nome_completo = "{$usuario['nome']} {$usuario['sobrenome']}";
 </head>
 
 <body>
-  <nav class="c-menu">
-    <div class="c-menu__user">
-      <img class="c-menu__user__image" src="/assets/images/default__user.png" alt="">
-      <p class="c-menu__user__name"><?php echo $nome_completo ?></p>
-    </div>
-    <div class="c-menu__links">
-      <a href="/"><button class="c-menu__link">Início</button></a>
-      <a href="/carteirinha"><button class="c-menu__link">Carteirinha</button></a>
-      <a href="/listar-usuarios"><button class="c-menu__link">Usuários</button></a>
-      <a href="/vacinas-adolescente"><button class="c-menu__link">Vacinas Adolescentes</button></a>
-      <a href="/vacinas-infantil"><button class="c-menu__link">Vacinas Infantil</button></a>
-      <a href="/logout"><button class="c-menu__link">Sair</button></a>
-    </div>
-  </nav>
+  <?= require './dashboard-menu.php' ?>
 
   <section class="c-section">
     <div class="c-section__title">
