9dd99939aebbbcab4e9f6bd69816df37ac022206
jonatasbaldan <jonatasbzc@gmail.com>
2024-05-21 20:10:58 -0300

    Change: Agrupado inputs para diminuição da interface. Trocado Titulo

--- vacinas-cadastro.php
+++ vacinas-cadastro.php
@@ -19,7 +19,7 @@ if ($_POST) {
   <link rel="stylesheet" href="/assets/css/components.css" />
   <link rel="stylesheet" href="/assets/css/dashboard.css" />
   <link rel="stylesheet" href="/assets/css/cadastro-vacina.css">
-  <title>Dashboard</title>
+  <title>Dashboard - Cadastrar Vacinas</title>
 </head>
 
 <body>
@@ -55,35 +55,38 @@ if ($_POST) {
             <input type="text" name="composicao" id="composicao" class="c-input"
               placeholder="Polissacarídeos capsulares purificados da Neisseria meningitidis do sorogrupo C" />
           </div>
+          <div class="c-input__group">
+            <div class="c-info__input">
+              <label for="numero_doses">Numero de doses<span class="c-input__required">*</span></label>
+              <input type="text" name="numero_doses" id="numero_doses" class="c-input" placeholder="2 doses" required />
+            </div>
 
-          <div class="c-info__input">
-            <label for="numero_doses">Numero de doses<span class="c-input__required">*</span></label>
-            <input type="text" name="numero_doses" id="numero_doses" class="c-input" placeholder="2 doses" required />
+            <div class="c-info__input">
+              <label for="intervalo_doses">Intervalo entre as doses</label>
+              <input type="text" name="intervalo_doses" id="intervalo_doses" class="c-input"
+                placeholder="60 dias entre a 1ª dose e 2ª dose, 60 dias entre a 2ª dose e o reforço" />
+            </div>
+
           </div>
 
-          <div class="c-info__input">
-            <label for="idade_recomendada">Idade recomendada<span class="c-input__required">*</span></label>
-            <input type="text" name="idade_recomendada" id="idade_recomendada" class="c-input"
-              placeholder="1ª dose: 3 meses, 2ª dose: 5 meses, Reforço: 12 meses" required />
-          </div>
+          <div class="c-input__group">
+            <div class="c-info__input">
+              <label for="idade_recomendada">Idade recomendada<span class="c-input__required">*</span></label>
+              <input type="text" name="idade_recomendada" id="idade_recomendada" class="c-input"
+                placeholder="1ª dose: 3 meses, 2ª dose: 5 meses, Reforço: 12 meses" required />
+            </div>
 
-          <div class="c-info__input">
-            <label for="intervalo_doses">Intervalo entre as doses</label>
-            <input type="text" name="intervalo_doses" id="intervalo_doses" class="c-input"
-              placeholder="60 dias entre a 1ª dose e 2ª dose, 60 dias entre a 2ª dose e o reforço" />
-          </div>
+            <div class="c-info__input">
+              <label for="reforco_recomendado">Reforço recomendado Minimo</label>
+              <input type="text" name="reforco_recomendado" id="reforco_recomendado" class="c-input"
+                placeholder="Reforço" />
+            </div>
 
+          </div>
           <div class="c-info__input">
             <label for="esquema_basico">Esquema básico</label>
             <input type="text" name="esquema_basico" id="esquema_basico" class="c-input" placeholder="30 dias" />
           </div>
-
-          <div class="c-info__input">
-            <label for="reforco_recomendado">Reforço recomendado Minimo</label>
-            <input type="text" name="reforco_recomendado" id="reforco_recomendado" class="c-input"
-              placeholder="Reforço" />
-          </div>
-
           <div class="c-info__input">
             <label for="tipo_etario">Tipo etario<span class="c-input__required">*</span></label>
             <select name="tipo_etario" id="tipo_etario" class="c-input c-input__select">
