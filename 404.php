b6a0efcf6c07f73b316327524fc691630f049a1b
jonatasbaldan <jonatasbzc@gmail.com>
2024-05-21 20:42:01 -0300

    Feature: Adicionado página 404

---
+++ 404.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 - Página não encontrada</title>
+</head>
+
+<body>
+    <h1>404</h1>
+    <p>Página não encontrada.</p>
+</body>
+
+</html>
