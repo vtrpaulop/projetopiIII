1e9b5b72ce492f5639d934de83921354002d5a93
jonatasbaldan <jonatasbzc@gmail.com>
2024-05-22 02:12:43 -0300

    Fix: Consertado diretórios do css

--- cadastro.php
+++ cadastro.php
@@ -62,9 +62,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="/assets/css/default.css" />
-    <link rel="stylesheet" href="/assets/css/components.css" />
-    <link rel="stylesheet" href="/assets/css/cadastro.css" />
+    <link rel="stylesheet" href="assets/css/default.css" />
+    <link rel="stylesheet" href="assets/css/components.css" />
+    <link rel="stylesheet" href="assets/css/cadastro.css" />
     <title>Cadastro</title>
 </head>
 
