e15d51ca42d39cbb653a4f14cf68449220279847
jonatasbaldan <jonatasbzc@gmail.com>
2024-05-25 13:30:17 -0300

    Fix: Organizado as rotas

--- routes.php
+++ routes.php
@@ -6,16 +6,21 @@ use Core\User;
 use Core\Colaborador;
 use Core\Supervisor;
 
+// Home
 $route->add('/', './index.html');
 $route->add('/cadastro', './cadastro.php', Guest::class);
 $route->add('/login', './login.php', Guest::class);
-$route->add('/logout', './logout.php', User::class);
-$route->add('/dashboard', './dashboard.php', User::class);
 
-// Dashboard
+// Usuario
+$route->add('/dashboard', './dashboard.php', User::class);
+$route->add('/logout', './logout.php', User::class);
+
+// Usuarios
 $route->add('/usuarios', './usuarios.php', Colaborador::class);
 $route->add('/usuarios-editar', './usuarios-editar.php', Colaborador::class);
-$route->add('/vacinas-cadastro', './vacinas-cadastro.php', Supervisor::class);
-$route->add('/vacinas', './vacinas.php', User::class);
-$route->add('/vacinas-editar', './vacinas-editar.php', Supervisor::class);
 $route->add('/usuarios-info', '/usuarios-info.php', Colaborador::class);
+
+// Vacinas
+$route->add('/vacinas', './vacinas.php', User::class);
+$route->add('/vacinas-cadastro', './vacinas-cadastro.php', Supervisor::class);
+$route->add('/vacinas-editar', './vacinas-editar.php', Supervisor::class);
